3649bb9f411c142a8f008753446ea0fdc2589265
arian <arian.aghajani01@gmail.com>
2025-08-28 13:08:02 +0330

    small fixes

--- frontend/src/games/XO/XoGame.tsx
+++ frontend/src/games/XO/XoGame.tsx
@@ -1,6 +1,7 @@
+import type { SessionSocket } from "../../lib/SessionWs"
+import { useXoGame } from "./UseXoGame"
+
 import { Game2PlayersCard } from "../../components/PlayersCard";
-import type { SessionSocket } from "../../lib/SessionWs";
-import { Board } from "./XoBoard";
 
 type XOGameProps = {
 	socketRef: React.RefObject<SessionSocket>
@@ -16,3 +17,48 @@ export default function XoGame({ socketRef }: XOGameProps) {
 }
 
 
+
+type BoardProps = {
+	socketRef: React.RefObject<SessionSocket>
+}
+export function Board({ socketRef }: BoardProps) {
+	const { squares, handleClick, lastPlayed } = useXoGame({ socketRef })
+
+	return (
+		<div
+			className="grid grid-cols-3 gap-1">
+			{squares.map((value, i) => (
+				<Square
+					key={i}
+					value={value == 1 ? "X" : value == 2 ? "O" : ""}
+					animate={lastPlayed === i}
+					onSquareClick={() => handleClick(i)} />
+			))}
+		</div>
+	);
+}
+type SquareProps = {
+	value: string | null;
+	onSquareClick: () => void;
+	animate: boolean;
+};
+
+
+function Square({ value, onSquareClick, animate }: SquareProps) {
+	return (
+		<button
+			onClick={onSquareClick}
+			className="w-20 h-20 hover:bg-gray-200
+			flex items-center justify-center border-2 border-gray-700
+			text-red-700 text-5xl font-bold
+			transition-all duration-200 ease-out
+			"
+		>
+			{value && (
+				<span className={animate ? "animate-pop" : ""}>
+					{value}
+				</span>
+			)}
+		</button>
+	);
+}
